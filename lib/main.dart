a0f7510aa8ba374392074a250c4a6178acc82c5b
AriellaYolaRifanda <ariellayola20@gmail.com>
2023-05-10 20:34:28 +0700

    pert_7

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("GridView"),
+          title: Text("Ariella Yola Rifanda"),
         ),
         body: Container(
             padding: const EdgeInsets.all(20.0),
